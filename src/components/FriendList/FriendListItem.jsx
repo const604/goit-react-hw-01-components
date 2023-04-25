80f8a685dd973c5f42eb5f0cefde4f53f8bb528d
const604 <116814907+const604@users.noreply.github.com>
2023-04-25 21:32:50 +0300

    7

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -3,11 +3,8 @@ import css from './FriendList.module.css';
 
 export const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
-    <li className={css.item} >
-      <span
-        className={css.status}
-        style={{ backgroundColor: isOnline ? 'green' : 'red' }}
-      ></span>
+    <li className={css.item}>
+      <span className={`${css.status} ${css[isOnline]}`}></span>
       <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
       <p className={css.name}>{name}</p>
     </li>
@@ -15,7 +12,7 @@ export const FriendListItem = ({ avatar, name, isOnline }) => {
 };
 
 FriendListItem.propTypes = {
-      avatar: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      isOnline: PropTypes.bool.isRequired,
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
-import getRandomHexColor from 'components/getRandomHexColor';
+import getRandomHexColor from 'Utils/getRandomHexColor';
 
 export const Statistics = ({ title, stats }) => {
   return (
